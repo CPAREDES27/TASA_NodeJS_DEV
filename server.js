07e9f65252eb3579d92a1887844c7c74614df956
pierotoscano <ptoscano@xternal.biz>
2022-01-25 21:03:33 -0500

    Pesca por embarcacion: exportar excel

--- server.js
+++ server.js
@@ -2040,6 +2040,27 @@ app.post('/api/sistemainformacionflota/PescaDescargada', cors(corsOptionsDelegat
 });
 
 /**
+ * Exportar excel Pesca por embarcación
+ */
+ app.post('/api/sistemainformacionflota/ExportPescaPorEmbarcacion', cors(corsOptionsDelegate),function (req, res) {  
+    console.log('Node server has been invoked. Now calling Backend service API ...');
+    _getAccessToken()
+    .then((result) => {
+        console.log('Successfully fetched OAuth access token: ' +  result.accessToken.substring(0,16));
+        var sUrl = HOST + "/api/sistemainformacionflota/ExportPescaPorEmbarcacion";
+        return _doQUERY(sUrl, result.accessToken, req.body, 'POST');
+    })
+    .then((result) => {
+        console.log('Successfully called OData service. Response body: ' + result.responseBody);
+        res.status(200).send(JSON.stringify(result.responseBody));
+    })
+    .catch((error) => {
+        console.log(error.message + ' Reason: ' + error.error);
+        res.status(500).send('ERROR: ' + error.message + ' - FULL ERROR: ' + error.error);
+    });
+});
+
+/**
  * Pesca de competencia - PRODUCE
  */
 app.post('/api/sistemainformacionflota/PescaCompetenciaProduce', cors(corsOptionsDelegate),function (req, res) {  
